16c97b69eab51be2ca1665ae1a0f8805cbbd5f4e
Glitj Katj <glitjkatj@gmail.com>
2023-07-04 11:57:33 -0500

    Refactor fizzbuzz.py for console messages

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,8 @@
+print("*" * 30)
+print("Hello! This is the FizzBuzzer!")
+print("*" * 30)
+print("""How this works: """)
+
 for num in range(1,101):
     if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
