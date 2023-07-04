3233a1827849a89193f3db633ed4739a3e21b9c3
Glitj Katj <glitjkatj@gmail.com>
2023-07-04 11:59:51 -0500

    Edit console messages

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,11 @@
-print("*" * 30)
+print("\n" + "*" * 30)
 print("Hello! This is the FizzBuzzer!")
-print("*" * 30)
-print("""How this works: """)
+print("*" * 30 + "\n")
+print("""How this works:
+You enter a starting number and an ending number.
+The multiples of 3 are Fizzed,
+And the multiples of 5 are Buzzed.
+Multiples of both are FizzBuzzed.\n""")
 
 for num in range(1,101):
     if num % 3 == 0 and num % 5 == 0:
