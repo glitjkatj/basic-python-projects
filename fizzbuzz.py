238d8c1c022e9fbd696340fbad67f106be434b24
Glitj Katj <glitjkatj@gmail.com>
2023-07-04 12:06:33 -0500

    Add comments to explain some code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -10,7 +10,10 @@ Multiples of both are FizzBuzzed.\n""")
 start_num = int(input("Enter an integer for the starting number: >> "))
 end_num = int(input("Enter an integer for the ending number: >> "))
 
+# for loop goes through the range of numbers
+# range() does not include the stop number, which is why there is a + 1
 for num in range(start_num, end_num + 1):
+    # conditional statements using modulus to check for multiples
     if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
     elif num % 3 == 0:
